98e6ac29f0b7ee3ee92fbe8813547acedd16aacc
Patrick Swickard <swickape@hotmail.com>
2023-12-11 09:27:35 -0500

    documentation for chug3.py lol why did i name it this

--- chug3.py
+++ chug3.py
@@ -29,16 +29,41 @@ sub = 'Spending_Description'
 #main = 'Vendor_Name'
 #sub = 'Amount'
 
+# okay, here's the thing...
+# we have two columns, a main column and a sub column
+# we are assuming that these columns have some sort of
+# controlled vocabulary, or at least very commonly 
+# recurring values that act as such
+# we are ultimately looking for rules of the form
+# IF COLUMN A HAS VALUE X THEN COLUMN B ALWAYS HAS VALUE Y
+# e.g.
+# IF CITY HAS VALUE BALTIMORE THEN STATE ALWAYS HAS VALUE MARYLAND
+# or slight generalizations of this
+# in terms of below we have
+# IF COLUMN MAIN HAS VALUE X THEN COLUMN SUB ALWAYS HAS VALUE Y
+# which happens when the sets in set_hash have cardinality 1
+# (better variable names may suggest themselves after batting this around)
+# the code below will crudely achieve this, marching through indices
+# for main and sub
+# for every value in the main column we create a set corresponding to that value
+# that has the corresponding value in the sub column
+# and then we go back through and for every keyword from main
+# we list the values we have encountered for sub
+# e.g. if main is vendor_name and sub is fund
+# then for each vendor we are reporting
+# the vendor and the set of funds that vendor is paid out of
+# not bad documentation for a file stupidly named chug3.py huh?
+# it's a pretty low bar
 
-service_hash = {}
+set_hash = {}
 for x in df.index:
   thismain = df[main][x]
   thissub = df[sub][x]
-  thismainset = service_hash.get(thismain,set())
+  thismainset = set_hash.get(thismain,set())
   thismainset.add(thissub)
-  service_hash[thismain] = thismainset
+  set_hash[thismain] = thismainset
 
-for thiskey in service_hash.keys():
+for thiskey in set_hash.keys():
   print(thiskey)
-  for thisval in service_hash[thiskey]:
+  for thisval in set_hash[thiskey]:
     print('***' + str(thisval))
